87e5ada79ebbdb8a8450d6063fc54fc55b1c9e8f
soloperfect <akhilchicku123@gmail.com>
2024-05-09 14:57:52 +0530

    routs creat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-
-
-
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Sign from "./components/Sign"
+import Signin from "./components/Signin"
+import Dashboard from "./components/Dashboard"
 
 export default function App() {
   return (
-    <h1 className="text 3xl text-red-500">app</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/signup" element={<Sign />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
